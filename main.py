88197cbca73881758250967986b933fa9782821e
LaurisSenkans <lauris.senkans@edu.rtu.lv>
2023-02-13 11:32:14 +0200

    o

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # python3
 
+
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
 
+
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
+
 
 
 def find_mismatch(text):
@@ -26,10 +29,13 @@ def find_mismatch(text):
         return opening_brackets_stack[0].position
     return "Success"
 
+
+
 def main():
     text = input()
     mismatch = find_mismatch(text)
     print(mismatch)
+
 
 if __name__ == "main":
     main()
